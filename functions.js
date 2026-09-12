2b091aeb060d684cb384d2de755f097af5a2ccd0
Santiago Pulido Castaño <santiagopulidocastan00@gmail.com>
2026-09-11 20:19:01 -0500

    Mejoras de vida

--- functions.js
+++ functions.js
@@ -1,5 +1,14 @@
-// Solicitudes
-function fun() {
-  alert("Enviado")
+let processScroll = () => {
+  let docElem = document.documentElement,
+      docBody = document.body,
+      scrollTop = docElem['scrollTop'] || docBody['scrollTop'],
+      scrollBottom = (docElem['scrollHeight'] || 
+  docBody['scrollHeight']) - window.innerHeight,
+      scrollPercent = scrollTop / scrollBottom * 100 + '%';
+
+        // console.log(scrollTop + ' / ' + scrollBottom + ' = ' + scrollPercent);
+
+  document.getElementById('progress-bar').style.setProperty('--scrollAmount', scrollPercent);
 }
 
+document.addEventListener('scroll', processScroll); 
